ee7efa5e4f9ac2e2644c376d7a031b4d6ed1b27c
Frank van Viegen <frank@paiq.nl>
2019-05-09 10:13:21 +0200

    Simplified RockCreator timing math

--- app/src/main/java/nl/saxion/playground/template/spaceshooter/RockCreator.java
+++ app/src/main/java/nl/saxion/playground/template/spaceshooter/RockCreator.java
@@ -8,20 +8,37 @@ public class RockCreator extends Entity {
 
     private Game game;
     private Random random = new Random();
-    private int tickCount;
+    private long tickCount;
+    private long nextRockTickCount;
 
-    public RockCreator(Game game) {
+    RockCreator(Game game) {
         this.game = game;
+        calculateNextRockTickCount();
     }
 
     @Override
     public void tick() {
-        float gameTime = (float)++tickCount / game.ticksPerSecond();
-        // start with one rock every 2s. every 1s after 2m. every 0.66s after 4m. etc.
-        float avgTimeBetweenRocks = 2f*120f / (120f+gameTime);
-        while (random.nextFloat() < (1f/avgTimeBetweenRocks) / game.ticksPerSecond() ) {
+        // Keep track of game time
+        ++tickCount;
+
+        // Should we span one (or more) rocks during this tick?
+        while (tickCount >= nextRockTickCount) {
             game.addEntity(new Rock(game));
+            calculateNextRockTickCount();
         }
+    }
+
+    private void calculateNextRockTickCount() {
+        // Calculate how long we've been playing, as average time between rocks
+        // should decrease based on this.
+        float gameTime = (float)++tickCount / game.ticksPerSecond();
+
+        // Start with one rock every 2s. every 1s after 2m. every 0.66s after 4m. etc.
+        float avgTimeBetweenRocks = 2f*120f / (120f+gameTime);
+
+        // The actual until the next rock will be 0 to 2 times the average time.
+        float nextRockTime = avgTimeBetweenRocks  * 2f * random.nextFloat();
+        nextRockTickCount = tickCount + Math.round(nextRockTime * game.ticksPerSecond());
     }
 }
 
