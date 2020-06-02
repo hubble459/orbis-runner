beba03b411e3fe2c14f9503531e8249142c12cce
Quentin Correia <481246@student.saxion.nl>
2020-06-02 14:04:08 +0200

    Player animation

--- app/src/main/java/nl/saxion/playground/orbisrunner/ui/StartScreenActivity.java
+++ app/src/main/java/nl/saxion/playground/orbisrunner/ui/StartScreenActivity.java
@@ -41,12 +41,12 @@ public class StartScreenActivity extends AppCompatActivity {
     private void init() {
         // Animate Player W A L K I N G
         ImageView player = findViewById(R.id.player);
-        ((AnimationDrawable) player.getDrawable()).start();
-        Animation.walkInCircle(player, 4000, new Random().nextBoolean());
+        ((AnimationDrawable) player.getBackground()).start();
+        Animation.walkInCircle(findViewById(R.id.rotateView), 7000, new Random().nextBoolean());
 
         // Set level title
         TextView lvl = findViewById(R.id.level);
-        lvl.setText(GameProvider.getLevel() /* get level from save instance */);
+        lvl.setText(GameProvider.getLevel() + "" /* get level from save instance */);
 
         // Assign Play Button
         Button playButton = findViewById(R.id.playButton);
