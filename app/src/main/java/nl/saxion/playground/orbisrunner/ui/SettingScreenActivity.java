ab00e8f5518600bf743a9fbd77b1b695d67a7d61
joost <447096@student.saxion.nl>
2020-06-05 11:56:24 +0200

    settings screen activity made

---
+++ app/src/main/java/nl/saxion/playground/orbisrunner/ui/SettingScreenActivity.java
@@ -0,0 +1,21 @@
+package nl.saxion.playground.orbisrunner.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import nl.saxion.playground.orbisrunner.R;
+
+public class SettingScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting_screen);
+
+        init();
+    }
+
+    public void init() {
+
+    }
+}
