807fd6a8ce1fe6c73a10c2b6b2048838501e56e2
Serwet <481909@student.saxion.nl>
2020-06-15 17:19:02 +0200

    Finish screen updated, errors fixed

--- app/src/main/java/nl/saxion/playground/orbisrunner/ui/FinishScreenActivity.java
+++ app/src/main/java/nl/saxion/playground/orbisrunner/ui/FinishScreenActivity.java
@@ -1,11 +1,12 @@
 package nl.saxion.playground.orbisrunner.ui;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import nl.saxion.playground.orbisrunner.R;
 import nl.saxion.playground.orbisrunner.game.Level;
@@ -14,25 +15,30 @@ import nl.saxion.playground.orbisrunner.singleton.GameProvider;
 public class FinishScreenActivity extends AppCompatActivity {
 
     private Level level;
-    private int collectableCoins;
+    private int collectibleCoins;
     private int collectedCoins;
     private int totalCoins;
     private int deathCounter;
-    private boolean objectiveClaimed = false;
+    private boolean objectiveClaimed;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_finish_screen);
 
-        checkObjective();
-
         level = GameProvider.getCurrentLevel();
-        collectableCoins = level.getCollectableCoins();
+        collectibleCoins = level.getCollectibleCoins();
         deathCounter = level.getDeathCounter();
+        objectiveClaimed = level.getObjectiveClaimed();
+        collectedCoins = level.getCollectedCoins();
+        totalCoins = GameProvider.getCoins();
 
         init();
 
+        checkObjective();
+
+        level.setCollectedCoins(0);
+        GameProvider.saveData(this);
     }
 
     private void init() {
@@ -46,25 +52,36 @@ public class FinishScreenActivity extends AppCompatActivity {
             }
         });
 
+        Button retryButton = findViewById(R.id.retryLevelButton);
+        retryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(FinishScreenActivity.this, GameActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        TextView totalCoinsText = findViewById(R.id.totalCoins);
+        totalCoinsText.setText(String.valueOf(collectedCoins));
+
+        TextView totalDeathsText = findViewById(R.id.totalDeaths);
+        totalDeathsText.setText(String.valueOf(deathCounter));
+
     }
 
     private void checkObjective() {
-        if (deathCounter == 0) {
-            if (collectedCoins == collectableCoins) {
-                objectiveCleared();
+        if (collectedCoins == collectibleCoins) {
+            if (!objectiveClaimed) {
+                GameProvider.setCoins(totalCoins + 5);
+                level.setObjectiveClaimed(true);
             }
+            objectiveCleared();
         }
     }
 
     private void objectiveCleared() {
         ImageView imageView = findViewById(R.id.completed);
         imageView.setImageResource(R.drawable.green_tick);
-
-        totalCoins = GameProvider.getCoins();
-        totalCoins = totalCoins + 5;
-
-        //level.setDeathCounter(0);
-
     }
 
 
