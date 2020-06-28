1c741cfa3729ddd37da0efe3e9eb54119e4566a5
OlafSonnemans <469740@student.saxion.nl>
2020-06-28 13:51:58 +0200

    uwu

--- app/src/main/java/nl/saxion/playground/orbisrunner/ui/LevelMaker.java
+++ app/src/main/java/nl/saxion/playground/orbisrunner/ui/LevelMaker.java
@@ -7,7 +7,6 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -281,7 +280,6 @@ public class LevelMaker extends AppCompatActivity {
     public void save(View view) {
         level.setScale(Entity.getScale());
         if (GameProvider.hasLevel(level)) {
-            Log.i("uwu", "save: " + level.getNumber());
             saveFinish();
         } else {
             int max = GameProvider.getLevels().size() + 1;
